8410b17590f51d36c1bb48b799916c6fabd440c6
w_liuli <w_liuli@kingsoft.com>
2025-11-13 15:53:20 +0800

    fix：添加用于测试同步云文档脚本

--- .github/scripts/sync_kdocs_test.py
+++ .github/scripts/sync_kdocs_test.py
@@ -1,7 +1,7 @@
 from wps_sdk import WPSClient
 
 client = WPSClient(api_key="SX20251113HXLMYW",api_secret="jVSbQeGbpmBNsAFibSEaAFdAwZuKoYWH")
-with open('README.md', 'r', encoding='utf-8') as f:
-content = f.read()
+
+content = "这是更新后的内容测试一下"
 new_content=content
 client.update_document(doc_id="cjpVZz0ASxGp",content=new_content)
